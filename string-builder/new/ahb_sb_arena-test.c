122b63cbbd286747be09fb3338090e2c05b1d9a6
Andre Herman <amelco.herman@gmail.com>
2025-03-05 18:03:06 -0300

    Finish adding capability (1st version)

--- string-builder/new/ahb_sb_arena-test.c
+++ string-builder/new/ahb_sb_arena-test.c
@@ -17,58 +17,42 @@ int main()
   assert(strcmp(cstr, teste.content) == 0);
   assert(cstr[teste.length] == '\0');
 
-  // string_append(&teste, " Ola Mundo!");
-  // assert(teste.length == 24);
-  // assert(teste.length < teste.capacity);
-  // 
-  // string_free(&teste);
-  // assert(teste.content == NULL);
-  // assert(teste.length == 0);
-  // assert(teste.capacity == 0);
-  // 
-  // String abc = string_new("ABC,D,EFKJH,alo,,dsa");
-  // StringList sl = string_split(abc, ',');
-  // assert(sl.size == 6);
-  // assert(strcmp(sl.items[0].content, "ABC") == 0);
-  // assert(strcmp(sl.items[1].content, "D") == 0);
-  // assert(strcmp(sl.items[2].content, "EFKJH") == 0);
-  // assert(strcmp(sl.items[3].content, "alo") == 0);
-  // assert(strcmp(sl.items[4].content, "") == 0);
-  // assert(strcmp(sl.items[5].content, "dsa") == 0);
+  string_append(&teste, " Ola Mundo!", &a, arena_add);
+  assert(teste.length == 24);
+  assert(teste.length < teste.capacity);
+   
+  String abc = string_new("ABC,D,EFKJH,alo,,dsa",&a, arena_add);
+  StringList sl = string_split(abc, ',', &a, arena_add);
+  assert(sl.size == 6);
+  assert(strcmp(sl.items[0].content, "ABC") == 0);
+  assert(strcmp(sl.items[1].content, "D") == 0);
+  assert(strcmp(sl.items[2].content, "EFKJH") == 0);
+  assert(strcmp(sl.items[3].content, "alo") == 0);
+  assert(strcmp(sl.items[4].content, "") == 0);
+  assert(strcmp(sl.items[5].content, "dsa") == 0);
 
-  // size_t index = string_next_index(&abc);
-  // assert(abc.index == IDX_NEVER_CALLED);
-  // index = string_index_of(&abc, ',');
-  // assert(index == 3);
-  // index = string_next_index(&abc);
-  // assert(index == 5);
-  // index = string_next_index(&abc);
-  // assert(index == 11);
-  // index = string_next_index(&abc);
-  // assert(index == 15);
-  // index = string_next_index(&abc);
-  // assert(index == 16);
-  // index = string_next_index(&abc);
-  // assert(index == IDX_NO_MORE_ENTRIES);
-  // index = string_next_index(&abc);
-  // assert(index == IDX_NO_MORE_ENTRIES);
+  size_t index = string_next_index(&abc);
+  assert(abc.index == IDX_NEVER_CALLED);
+  index = string_index_of(&abc, ',');
+  assert(index == 3);
+  index = string_next_index(&abc);
+  assert(index == 5);
+  index = string_next_index(&abc);
+  assert(index == 11);
+  index = string_next_index(&abc);
+  assert(index == 15);
+  index = string_next_index(&abc);
+  assert(index == 16);
+  index = string_next_index(&abc);
+  assert(index == IDX_NO_MORE_ENTRIES);
+  index = string_next_index(&abc);
+  assert(index == IDX_NO_MORE_ENTRIES);
 
-  // String substr = string_substring(abc, 6, 11);
-  // assert(strcmp(substr.content, "EFKJH") == 0);
-  // string_free(&substr);
+  String substr = string_substring(abc, 6, 11, &a, arena_add);
+  assert(strcmp(substr.content, "EFKJH") == 0);
 
-  // String substr_fail = string_substring(abc, 11, 6);
-  // assert(substr.length == 0);
-  // string_free(&substr_fail);
-  // 
-  // string_free(&abc);
-  // assert(abc.content == NULL);
-  // assert(abc.length == 0);
-  // assert(abc.capacity == 0);
-
-  // stringlist_free(&sl);
-  // assert(sl.items == NULL);
-  // assert(sl.size == 0);
+  String substr_fail = string_substring(abc, 11, 6, &a, arena_add);
+  assert(substr_fail.length == 0);
 
   printf("All tests completed succesfully.\n");
   arena_free(&a);
--- string-builder/new/ahb_sb_arena.h
+++ string-builder/new/ahb_sb_arena.h
@@ -52,15 +52,29 @@ String string_new(char* text
 void   string_free(String* str);
 #endif // ARENA
 
-void   string_append(String* str, char* text);
+void   string_append(String* str, char* text
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        );
+
 size_t string_index_of(String* str, char c);
 size_t string_next_index(String* str);                        // Returns the next index of the previously called string_index_of(). Calls string_index_of() if it wasn't called before.
                                                               // No more characters: returns IDX_NO_MORE_ENTRIES
-String string_substring(String str, size_t ini, size_t end);  // ini (inclusive), end (exclusive)
+String string_substring(String str, size_t ini, size_t end    // ini (inclusive), end (exclusive)
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        );
+
 //String string_concat(String str1, ...);  TODO: concatenation function with variadic arguments (all String type)
 char*  string_to_cstr(String str);
 
-StringList string_split(String str, char separator);
+StringList string_split(String str, char separator
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        );
 void       stringlist_free(StringList* str_list);
 
 #endif //AHB_SB_H
@@ -118,110 +132,156 @@ void string_free(String* str) {
   str->content = NULL;
 }
 
-// void string_append(String* str, char* text) {
-//   size_t text_len = cstring_len(text);
-//   size_t new_len = text_len + str->length;
-//   bool need_realloc = new_len > str->capacity;
-//   while (new_len > str->capacity) {
-//     printf("len: %zu  > cap: %zu\n", new_len, str->capacity);
-//     str->capacity *= 2;
-//     if (str->capacity > MAX_CAPACITY) panic("String is too long (> 1 GB)");
-//   }
-// 
-//   if (need_realloc) {
-//     str->content = realloc(str->content, str->capacity);
-//     assert(str->content && "Buy more RAM");
-//   }
-//   memcpy(str->content + str->length, text, cstring_len(text));
-//   str->length = new_len;
-// }
-// 
-// size_t __private_string_index_of(String str, size_t start, char c) {
-//   for (size_t i = start; i < str.length; ++i) {
-//     if (str.content[i] == c)
-//       return i;
-//   }
-//   return IDX_NO_MORE_ENTRIES;
-// }
-// 
-// // must be called first when searching for the same char several times
-// size_t string_index_of(String* str, char c) {
-//   str->index = IDX_NEVER_CALLED;
-//   str->last_searched_char = c;
-//   str->index = __private_string_index_of(*str, 0, c);
-//   return str->index;
-// }
-// 
-// // must be called after calling string_index_of when wanted to look for other occurrencies of the same char in the same string. If search function never called before, call it.
-// size_t string_next_index(String* str) {
-//   if (str->index == IDX_NEVER_CALLED)
-//     return IDX_NEVER_CALLED;
-//   str->index = __private_string_index_of(*str, str->index + 1, str->last_searched_char);
-//   return str->index;
-// }
-// 
-// // returns a substring given a initial (inclusive) and end (exclusive) indices.
-// // returns NULL if fails
-// // must be free'd
-// // TODO: it is not memory sanitized when compiling with memory sanitization. Fix it!
-// String string_substring(String str, size_t ini, size_t end) {
-//   if (ini >= end || ini < 0 || end > str.length) return string_new("");
-//   size_t size = end - ini;
-// 
-//   String new_string = {0};
-//   new_string.length = size;
-//   new_string.capacity = INITIAL_CAPACITY;
-//   new_string.content = calloc(INITIAL_CAPACITY, sizeof(char));
-//   assert(new_string.content && "Buy more RAM");
-// 
-//   memcpy(new_string.content, str.content + ini, size);
-//   new_string.content[size] = '\0';
-//   return new_string;
-// }
+void string_append(String* str, char* text
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        ) {
+  size_t text_len = cstring_len(text);
+  size_t new_len = text_len + str->length;
+  bool need_realloc = new_len > str->capacity;
+  while (new_len > str->capacity) {
+    printf("len: %zu  > cap: %zu\n", new_len, str->capacity);
+    str->capacity *= 2;
+    if (str->capacity > MAX_CAPACITY) panic("String is too long");
+  }
+
+  if (need_realloc) {
+#ifdef ARENA
+    str->content = cb(a, str->capacity);
+#else
+    str->content = realloc(str->content, str->capacity);
+#endif
+    assert(str->content && "Buy more RAM");
+  }
+  memcpy(str->content + str->length, text, cstring_len(text));
+  str->length = new_len;
+}
+
+size_t __private_string_index_of(String str, size_t start, char c) {
+  for (size_t i = start; i < str.length; ++i) {
+    if (str.content[i] == c)
+      return i;
+  }
+  return IDX_NO_MORE_ENTRIES;
+}
+
+// must be called first when searching for the same char several times
+size_t string_index_of(String* str, char c) {
+  str->index = IDX_NEVER_CALLED;
+  str->last_searched_char = c;
+  str->index = __private_string_index_of(*str, 0, c);
+  return str->index;
+}
+
+// must be called after calling string_index_of when wanted to look for other occurrencies of the same char in the same string. If search function never called before, call it.
+size_t string_next_index(String* str) {
+  if (str->index == IDX_NEVER_CALLED)
+    return IDX_NEVER_CALLED;
+  str->index = __private_string_index_of(*str, str->index + 1, str->last_searched_char);
+  return str->index;
+}
+ 
+// returns a substring given a initial (inclusive) and end (exclusive) indices.
+// returns NULL if fails
+// must be free'd
+// TODO: it is not memory sanitized when compiling with memory sanitization. Fix it!
+String string_substring(String str, size_t ini, size_t end
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        ) {
+  if (ini >= end || ini < 0 || end > str.length) { 
+      return string_new(""
+#ifdef ARENA
+              , a, cb
+#endif
+          );
+  }
+  size_t size = end - ini;
+
+  String new_string = {0};
+  new_string.length = size;
+  new_string.capacity = INITIAL_CAPACITY;
+#ifdef ARENA
+  new_string.content = cb(a, INITIAL_CAPACITY * sizeof(char));
+#else
+  new_string.content = calloc(INITIAL_CAPACITY, sizeof(char));
+#endif
+  assert(new_string.content && "Buy more RAM");
+
+  memcpy(new_string.content, str.content + ini, size);
+  new_string.content[size] = '\0';
+  return new_string;
+}
 
 char* string_to_cstr(String str) {
   return str.content;
 }
  
-// StringList string_split(String str, char separator) {
-//   int num_elems = 1;
-//   for (int i = 0; i < str.length; ++i) {
-//     if (str.content[i] == separator) {
-//       num_elems++;    
-//     }
-//   }
-//   
-//   String* splits = malloc(num_elems * sizeof(String));
-//   assert(splits && "Buy more RAM");
-// 
-//   int index = 0;
-//   int last_index = 0;
-//   char tmp_text[str.length];
-//   for (int i = 0; i < str.length; ++i) {
-//     if (str.content[i] == separator) {
-//       if (tmp_text[0] == '\0') splits[index] = string_new("");
-//       else splits[index] = string_new(tmp_text);
-//       index++;
-//       tmp_text[0] = '\0';
-//       last_index = i + 1;
-//       continue;
-//     }
-//     tmp_text[i - last_index] = str.content[i];
-//     tmp_text[i - last_index + 1] = '\0';
-//   }
-// 
-//   if (cstring_len(tmp_text) > 0) {
-//     splits[index] = string_new(tmp_text);
-//   }
-// 
-//   StringList list = {
-//     .items = splits,
-//     .size = num_elems,
-//   };
-//   
-//   return list;
-// }
-// 
+StringList string_split(String str, char separator
+#ifdef ARENA
+        , void* a, void* (*cb)(Arena*, size_t)
+#endif
+        ) {
+  int num_elems = 1;
+  for (int i = 0; i < str.length; ++i) {
+    if (str.content[i] == separator) {
+      num_elems++;    
+    }
+  }
+  
+#ifdef ARENA
+  String* splits = cb(a, num_elems * sizeof(String));
+#else
+  String* splits = malloc(num_elems * sizeof(String));
+#endif
+  assert(splits && "Buy more RAM");
+
+  int index = 0;
+  int last_index = 0;
+  char tmp_text[str.length];
+  for (int i = 0; i < str.length; ++i) {
+    if (str.content[i] == separator) {
+      if (tmp_text[0] == '\0') {
+          splits[index] = string_new(""
+#ifdef ARENA
+                  , a, cb
+#endif
+                  );
+      }
+      else {
+          splits[index] = string_new(tmp_text
+#ifdef ARENA
+                  , a, cb
+#endif
+                  );
+      }
+      index++;
+      tmp_text[0] = '\0';
+      last_index = i + 1;
+      continue;
+    }
+    tmp_text[i - last_index] = str.content[i];
+    tmp_text[i - last_index + 1] = '\0';
+  }
+
+  if (cstring_len(tmp_text) > 0) {
+    splits[index] = string_new(tmp_text
+#ifdef ARENA
+                  , a, cb
+#endif
+            );
+  }
+
+  StringList list = {
+    .items = splits,
+    .size = num_elems,
+  };
+  
+  return list;
+}
+ 
 // void stringlist_free(StringList* str_list) {
 //   for (int i = 0; i < str_list->size; ++i) {
 //     free(str_list->items[i].content);
